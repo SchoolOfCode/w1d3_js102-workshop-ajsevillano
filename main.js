d43ee7ba8be042eb3c0042621b38e8139522badb
Sevi <correosevi@gmail.com>
2021-11-17 20:51:42 +0000

    Step 4

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 let attempts = 0;
 
-while (attempts < 3) {
-  let userPassword = prompt('Please enter the password.');
-  if (userPassword === 'myPassword1!') {
-    console.log('the password is correct!');
-    break;
-  } else {
-    ++attempts;
-    console.log('the password is incorrect!');
+// while (attempts < 3) {
+//   let userPassword = prompt('Please enter the password.');
+//   if (userPassword === 'myPassword1!') {
+//     alert('Your secret name is: pigeon');
+//     break;
+//   } else {
+//     ++attempts;
+//     alert('the password is incorrect!');
+//   }
+// }
+
+function authenticateUser() {
+  while (attempts < 3) {
+    let userPassword = prompt('Please enter the password.');
+    if (userPassword === 'myPassword1!') {
+      return true;
+    } else {
+      ++attempts;
+    }
   }
+  return false;
+}
+
+let isLoggedIn = authenticateUser();
+if (isLoggedIn) {
+  console.log('your secret name is Mr Pigeon');
 }
